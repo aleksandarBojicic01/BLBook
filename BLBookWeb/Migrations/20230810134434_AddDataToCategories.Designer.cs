01dd915dda740b402cfdbb6a6ab5e08de2bb1504
Aleksandar Bojicic <aleksandar.bojicic01@gmail.com>
2023-08-10 15:45:43 +0200

    Add data to Categories

--- BLBookWeb/Migrations/20230810134434_AddDataToCategories.Designer.cs
+++ BLBookWeb/Migrations/20230810134434_AddDataToCategories.Designer.cs
@@ -3,6 +3,7 @@ using BLBookWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BLBookWeb.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230810134434_AddDataToCategories")]
+    partial class AddDataToCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
