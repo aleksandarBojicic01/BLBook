bc364061a6a0787951f272c8f052dcc8d673bf90
Aleksandar Bojicic <aleksandar.bojicic01@gmail.com>
2023-08-11 16:36:09 +0200

    Add categories table to Category Index, change bootstrap theme to Lux

--- BLBookWeb/Controllers/CategoryController.cs
+++ BLBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLBookWeb.Data;
+using BLBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BLBookWeb.Controllers
 {
 	public class CategoryController : Controller
 	{
+		private readonly AppDbContext _db;
+
+		public CategoryController(AppDbContext db)
+		{
+			_db = db;
+		}
 		public IActionResult Index()
 		{
-			return View();
+			List<Category> categories = _db.Categories.ToList();
+			return View(categories);
 		}
 	}
 }
