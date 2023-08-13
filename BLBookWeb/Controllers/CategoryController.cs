83d36012dd6014f543035f395e4290076b94ea78
Aleksandar Bojicic <aleksandar.bojicic01@gmail.com>
2023-08-13 11:33:13 +0200

    Finish create, edit and delete functions for categories

--- BLBookWeb/Controllers/CategoryController.cs
+++ BLBookWeb/Controllers/CategoryController.cs
@@ -22,5 +22,71 @@ namespace BLBookWeb.Controllers
 		{
 			return View();
 		}
+
+		[HttpPost]
+		public IActionResult Create(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(category);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			Category? category = _db.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(category);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			Category? category = _db.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+
+		[HttpPost]
+		public IActionResult Delete(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Remove(category);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
 	}
 }
