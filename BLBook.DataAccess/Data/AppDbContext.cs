7699d2ceab8ca0986e3e031a98f9eaf87be4740d
Aleksandar Bojicic <aleksandar.bojicic01@gmail.com>
2023-08-21 12:15:08 +0200

    Add identity scaffolding

--- BLBook.DataAccess/Data/AppDbContext.cs
+++ BLBook.DataAccess/Data/AppDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using BLBook.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BLBook.DataAccess.Data
 {
-	public class AppDbContext : IdentityDbContext
-	{
+	public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
 		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
 		{
 			
--- BLBookWeb/Program.cs
+++ BLBookWeb/Program.cs
@@ -2,6 +2,7 @@ using BLBook.DataAccess.Data;
 using BLBook.DataAccess.Repository.IRepository;
 using BLBook.DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
